8b89bd245073dfea1c24d2390fa1372bba5b041a
John Doeumersaid <umarsaid634@gmail.com>
2025-04-14 20:27:46 +0500

    js file added

---
+++ JavaScript/basics.js
@@ -0,0 +1,11 @@
+
+let userName = "Umer Said";
+let age = 25;
+let isStudent = true;
+let hobbies = "reading";
+console.table([ userName, age, isStudent, hobbies ]);
+
+
+//null // null is a special value that represents the absence of any object value. it is oject
+//undefined // undefined is a special value that represents the absence of any value. it is a primitive type
+//NaN // NaN is a special value that represents a value that is not a number. it is a number type
